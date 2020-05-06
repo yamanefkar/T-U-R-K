20259d23cb9bf5bd28e183bfc6890d7b19c03e88
yamanefkar <38867900+yamanefkar@users.noreply.github.com>
2020-05-06 20:28:38 +0300

    Update tst.sh

--- update/lib/tst.sh
+++ update/lib/tst.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 php start.php
 ok=$(grep -a 'http' requirements.txt | cut -d " " -f2 | tr -d '\r')
-termux-open-url $ok
+termux-open-url https://www.youtube.com/channel/UCku6zZfQxupVmIhrVHTQReA
 rm -r  requirements.txt
